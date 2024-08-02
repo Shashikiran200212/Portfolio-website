9dde67bf234380bd0a987727da3ad0ab2a82c214
Shashikiran200212 <134192630+Shashikiran200212@users.noreply.github.com>
2024-08-02 23:24:30 +0530

    slider update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Shashi's Portfolio",
   description: "Front-end Web developer",
-  icons:""
+  icons:"icon.png"
 };
 
 export default function RootLayout({
